7f0e63ed9d531c170b0d1042aa4d676c834dcf75
buoumall <buoumall@buoumalldeimac2.lan>
2017-01-20 14:00:17 +0800

    Initial Commit

---
+++ MVVMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMDemo
+//
+//  Created by buoumall on 2017/1/20.
+//  Copyright © 2017年 buoumall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVMDemo
+//
+//  Created by buoumall on 2017/1/20.
+//  Copyright © 2017年 buoumall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
